cf14a092f71e76ea0def31a688ace849d2eed601
yaphtes <yaphtes.ks@gmail.com>
2018-01-13 00:10:15 +0400

    Home layout created

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './footer.css';
+
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <div className="wrap">
+        <span>@ 2018 instagram</span>
+        <span>Maked by Konstantin Otpushchennikov</span>
+      </div>
+    </footer>
+  );
+}
--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,12 +1,22 @@
 import React from 'react';
 import Header from '../Header';
 import About from './About';
+import Posts from './Posts';
+import Footer from '../Footer';
+
+import post1 from './Posts/assets/post1.png';
+import post2 from './Posts/assets/post2.png';
+import post3 from './Posts/assets/post3.png';
+import post4 from './Posts/assets/post4.png';
+import post5 from './Posts/assets/post5.png';
+
 
 export default function Home() {
   const username = 'username';
   const name = 'Firstname';
   const surname = 'Lastname';
   const text = 'Lorem Ipsum - это текст рыба, часто используемый в печати вэб-дизайне. Lorem Ipsum является стандартной http://website.com';
+  const posts = [post1, post2, post3, post4, post5, post1, post2, post3, post4, post5, post1, post2];
 
   return (
     <div className="home">
@@ -17,6 +27,8 @@ export default function Home() {
         surname={surname}
         text={text}
       />
+      <Posts posts={posts} />
+      <Footer />
     </div>
   );
 }
